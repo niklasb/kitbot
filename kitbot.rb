17a47f9fc924f59cad1e769dffab51608dce77fb
Niklas Baumstark <niklas.baumstark@gmail.com>
2012-04-06 23:48:50 +0200

    restructuring

--- kitbot.rb
+++ kitbot.rb
@@ -22,19 +22,7 @@ $channels = ["#kitinfo"]
 config = load_yaml_hash($config_file)
 bot = IrcBot.new($nick)
 
-bot.add_command /^.time$/, '.time' do |bot, where, from|
-  bot.say "Time: %s" % Time.now, where
-end
-
-agent = Mechanize.new
-agent.user_agent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)"
-bot.add_command /(https?:\/\/\S+)/, 'HTTP URLs (will fetch title)' do |bot, where, from, url|
-  page = agent.get(url)
-  title = page.at('title').text.gsub(/\s+/, ' ').strip
-  bot.say "Title: %s" % title, where
-end
-
-# implement logging, as the substitution mechanism and stats depend on it :)
+# set up logging, as the substitution mechanism and stats depend on it :)
 history = Hash.new { |h,k| h[k] = [] }
 user_stats = Hash.new { |h,k| h[k] = Hash.new { |h,k| h[k] = { :letter_count => 0 }}}
 # merge data from file
@@ -44,6 +32,7 @@ user_stats = Hash.new { |h,k| h[k] = Hash.new { |h,k| h[k] = { :letter_count => 
   end
 end
 
+# log
 bot.add_command /(.*)/ do |bot, where, from, msg|
   chanhist = history[where]
   chanhist << [Time.now, from, msg]
@@ -58,6 +47,10 @@ bot.add_command /(.*)/ do |bot, where, from, msg|
   stats[:last_seen] = Time.now
 end
 
+# Commands
+#========================
+
+# highscore by letter count
 bot.add_command /^.stats$/, '.stats' do |bot, where|
   users_count = user_stats[where].map { |user, stats| [user, stats[:letter_count]] }
   top = users_count.sort_by { |user, count| -count }[0..$top_users]
@@ -65,15 +58,7 @@ bot.add_command /^.stats$/, '.stats' do |bot, where|
   bot.say "Top users (letter count-wise): %s" % str, where
 end
 
-bot.add_command /^s?\/([^\/]*)\/([^\/]*)\/?$/, 's/x/y/ substitution' do |bot, where, from, pattern, subst|
-  pattern = Regexp.new(pattern)
-  result = history[where].reverse.drop(1).find { |_, f, m| f == from && m =~ pattern }
-  if result
-    time, from, msg = result
-    bot.say "<%s> %s" % [from, msg.gsub(pattern, subst)], where
-  end
-end
-
+# single user stats
 bot.add_command /^.seen\s+(\S+)$/, '.seen' do |bot, where, from, query|
   result = user_stats[where].find { |name, _| name.include?(query) }
   if result
@@ -86,14 +71,36 @@ bot.add_command /^.seen\s+(\S+)$/, '.seen' do |bot, where, from, query|
   end
 end
 
+# fetch the title of pasted URLs
+agent = Mechanize.new
+agent.user_agent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)"
+bot.add_command /(https?:\/\/\S+)/, 'HTTP URLs (will fetch title)' do |bot, where, from, url|
+  page = agent.get(url)
+  title = page.at('title').text.gsub(/\s+/, ' ').strip
+  bot.say "Title: %s" % title, where
+end
+
+# enable use of s/foo/bar syntax to correct mistakes
+bot.add_command /^s?\/([^\/]*)\/([^\/]*)\/?$/, 's/x/y/ substitution' do |bot, where, from, pattern, subst|
+  pattern = Regexp.new(pattern)
+  result = history[where].reverse.drop(1).find { |_, f, m| f == from && m =~ pattern }
+  if result
+    time, from, msg = result
+    bot.say "<%s> %s" % [from, msg.gsub(pattern, subst)], where
+  end
+end
+
+# don't forget to write stats back to config
+at_exit do
+  open($config_file, 'wb') { |f| f.write({ :user_stats => user_stats }.to_yaml) }
+end
+
+# start bot in background
 Thread.new(bot) do |bot|
   bot.connect($server)
   $channels.each { |chan| bot.join(chan) }
   bot.main_loop
 end
 
-# interactive shell
+# start an interactive shell in the main thread :)
 binding.pry
-
-# write data to config
-open($config_file, 'wb') { |f| f.write({ :user_stats => user_stats }.to_yaml) }
