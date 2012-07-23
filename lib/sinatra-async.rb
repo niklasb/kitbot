20880785eac921551cf57e602d521b8908ab81de
Niklas Baumstark <niklas.baumstark@gmail.com>
2012-07-24 00:49:17 +0200

    fix sinatra-async for regex routes

--- lib/sinatra-async.rb
+++ lib/sinatra-async.rb
@@ -10,7 +10,12 @@ module Sinatra
     %w(get post head options put).each do |method|
       define_method(method) do |path, opts={}, &bk|
         super(path, opts, &bk)
-        super('/async' + path, opts) do
+        new_path = if path.is_a? Regexp
+                     /\/async#{path.source}/
+                   else
+                     '/async' + path
+                   end
+        super(new_path, opts) do
           EM.defer(&bk)
           nil
         end
